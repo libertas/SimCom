47f1ebbd0015cdb73810673b1b7d8d10aba1e93f
libertas <horizonvei@gmail.com>
2017-03-10 10:03:31 +0800

    Use less global variables to make it stable

--- DataLinkLayer.c
+++ DataLinkLayer.c
@@ -2,8 +2,8 @@
 #include "DataLinkLayer.h"
 #include "PhysicalLayer.h"
 
+// Buffer Format:
 // [STX] [CRC] [DATA[n]] [ETX]
-char dl_buf[DL_BUF_LEN];
 
 
 bool dl_init()
@@ -13,6 +13,8 @@ bool dl_init()
 
 bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
 {
+  char dl_receive_buf[DL_BUF_LEN];
+  
   SIMCOM_LENGTH_TYPE length_tmp;
   char c;
 
@@ -35,19 +37,19 @@ bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
     if(!start_found) {
       if(c == 0x02) {
         start_found = true;
-        dl_buf[i] = c;
+        dl_receive_buf[i] = c;
         i++;
         continue;
       }
     } else {
       if(i < 2) {
-        dl_buf[i] = c;
+        dl_receive_buf[i] = c;
         i++;
         continue;
       } else {
         if(esc_found) {
           esc_found = false;
-          dl_buf[i] = c;
+          dl_receive_buf[i] = c;
           i++;
           continue;
         } else {
@@ -55,7 +57,7 @@ bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
             esc_found = true;
             continue;
           } else if(c == 0x02) {
-            dl_buf[0] = c;
+            dl_receive_buf[0] = c;
             i = 1;
             start_found = true;
             esc_found = false;
@@ -67,7 +69,7 @@ bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
             i = 0;
             break;
           } else {
-            dl_buf[i] = c;
+            dl_receive_buf[i] = c;
             i++;
             continue;
           }
@@ -81,9 +83,9 @@ bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
     end_found = false;
     esc_found = false;
 
-    if(verify(dl_buf + 2, length_tmp - 2) == dl_buf[1]) {
+    if(verify(dl_receive_buf + 2, length_tmp - 2) == dl_receive_buf[1]) {
       for(SIMCOM_LENGTH_TYPE tmp = 2; tmp < length_tmp; tmp++) {
-        data[tmp - 2] = dl_buf[tmp];
+        data[tmp - 2] = dl_receive_buf[tmp];
       }
       *length = length_tmp - 2;
       return true;
@@ -95,6 +97,8 @@ bool dl_receive(char *data, SIMCOM_LENGTH_TYPE *length)
 
 bool dl_send(char *data, SIMCOM_LENGTH_TYPE length)
 {
+  char dl_send_buf[DL_BUF_LEN];
+
   SIMCOM_LENGTH_TYPE i, j;
 
   if((SIMCOM_DLENGTH_TYPE)(length << 1) + 3 > DL_BUF_LEN) {
@@ -102,25 +106,25 @@ bool dl_send(char *data, SIMCOM_LENGTH_TYPE length)
   }
 
   // STX
-  dl_buf[0] = 0x02;
-  dl_buf[1] = verify(data, length);
+  dl_send_buf[0] = 0x02;
+  dl_send_buf[1] = verify(data, length);
 
   for(i = 0, j = 2; i < length; i++, j++) {
     if(data[i] <= 0x1F) {
-      dl_buf[j] = 0x1B;
+      dl_send_buf[j] = 0x1B;
       j++;
     }
 
-    dl_buf[j] = data[i];
+    dl_send_buf[j] = data[i];
   }
 
   // ETX
-  dl_buf[j] = 0x03;
+  dl_send_buf[j] = 0x03;
 
   for(i = 0; i < j + 1; i++) {
     SIMCOM_LENGTH_TYPE count = 0;
 
-    while(!ph_send(dl_buf[i])) {
+    while(!ph_send(dl_send_buf[i])) {
       if(count > DL_RETRY_TIMES) {
         return false;
       }
